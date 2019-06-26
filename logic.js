2197118798d0f651e8bf4786bbc3c2b0912be081
Amarius1 <amarius2000@gmail.com>
2019-06-26 19:28:37 +0300

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,14 +1,1 @@
 
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-
-    if (scroll >= 120) {
-        $(".title").addClass("blur");
-    } else {
-        $(".title").removeClass("blur");
-    }
-});
-new Tooltip(referenceElement, {
-    placement: 'top', // or bottom, left, right, and variations
-    title: "Top"
-});
